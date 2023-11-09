0d27ea659b20cf4808d8ee272261f91923c67953
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-11-09 23:42:26 +0300

    :feat: add cors opiton and default route

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require("express");
-const { Client } = require("@googlemaps/google-maps-services-js");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const { createHash, randomBytes } = require("crypto");
-const { Email } = require("./email/email");
+const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const mime = require("mime-types");
+const dotenv = require("dotenv");
+const { Client } = require("@googlemaps/google-maps-services-js");
+const { createHash, randomBytes } = require("crypto");
+const { Email } = require("./email/email");
 const { Upload, uploadFile } = require("./uploadFile");
 
+dotenv.config();
+const client = new Client({});
+const app = express();
+
+// const CyclicDB = require("@cyclic.sh/dynamodb");
+// const db = CyclicDB("dull-lime-cod-robeCyclicDB");
 const CyclicDb = require("@cyclic.sh/dynamodb");
 const db = CyclicDb("successful-ant-zipperCyclicDB");
-
-dotenv.config();
-
-const app = express();
-const client = new Client({});
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const signAccessToken = (key) => {
   const jwtSecret = "wzPe7g19Yan27T2ATud1Kw==";
@@ -28,6 +25,42 @@ const signAccessToken = (key) => {
   });
 };
 
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(express.static('public', options))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`index.js listening on ${port}`);
+});
+app.get("/", async (req, res) => {
+  // testing code on server
+  res.json({ msg: "hello world" }).end();
+});
+
+app.post("/api", async (req, res) => {
+  const { name, email } = req.body;
+  const result = await db.putItem({ name, email });
+  res.json(result).end();
+});
+
+// app.post('/:col/:key', async (req, res) => { // deleting a key from a collection
+//   console.log(req.body)
+
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).set(key, req.body)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
 app.post("/clients/new", async (req, res) => {
   const { email, password } = req.body;
 
@@ -65,79 +98,51 @@ app.post("/clients/new", async (req, res) => {
   }
 });
 
-app.post("/users/login", async (req, res) => {
-  const { email, password } = req.body;
+// Get hospitals/health facilities within given radius using googlemaps
+app.get("/near-by-places", async (req, res) => {
+  console.log("req.query", req.query);
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+  const disease = req.query.disease;
 
   try {
-    // Fetching the client with the provided email
-    const client = await db.collection("doceaseclients").get(email);
-    if (client) {
-      const passwordMatch = await bcrypt.compare(
-        password,
-        client.props.password
-      );
-      if (!passwordMatch) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Invalid password entered." });
-      } else {
-        // return res.status(401).json({ success: false, message: 'Invalid email or password.' });
-        const accessToken = signAccessToken(client.key);
-        return res.status(200).json({
-          success: true,
-          message: "Login successful.",
-          data: { client },
-          accessToken: accessToken,
-        });
-      }
-    } else {
-      return res.status(401).json({
+    if (!latitude || !longitude) {
+      return res.status(400).json({
         success: false,
-        message: `Email adress not found, if you don't have an account please sign up.`,
+        message: "Please provide location co-ordinates",
       });
     }
+    if (!disease) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide a disease",
+      });
+    }
+    const healthFacilities = await client.placesNearby({
+      params: {
+        location: `${latitude}, ${longitude}`,
+        key: process.env.GOOGLE_MAPS_API_KEY,
+        radius: 1000,
+        types: ["hospital", "health"],
+      },
+    });
+    if (healthFacilities.statusText !== "OK") {
+      return res
+        .status(400)
+        .json({ success: false, message: "could not find places" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "get health successfully",
+      data: healthFacilities.data,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 });
 
-app.get("/near-by-places", async (req, res) => {
-  console.log("req.query");
-  console.log(req.query);
-  // { latitude: '0.3244032', longitude: '32.587776' }
-  const latitude = req.query.latitude;
-  const longitude = req.query.longitude;
-
-  if (!latitude || !longitude) {
-    return res.status(400).json({
-      success: false,
-      message: "Please provide location co-ordinates",
-    });
-  }
-
-  const healthFacilities = await client.placesNearby({
-    params: {
-      // location: "0.3244032, 32.587776",
-      location: `${latitude}, ${longitude}`,
-      key: process.env.GOOGLE_MAPS_API_KEY,
-      radius: 1000,
-      types: ["hospital", "health"],
-    },
-  });
-
-  if (healthFacilities.statusText !== "OK") {
-    return res
-      .status(400)
-      .json({ success: false, message: "could not find places" });
-  }
-
-  res.status(200).json({
-    success: true,
-    message: "get health successfully",
-    data: healthFacilities.data,
-  });
-});
 app.post("/users/forgot-password", async (req, res) => {
   try {
     const email = req.body.email;
@@ -150,7 +155,7 @@ app.post("/users/forgot-password", async (req, res) => {
     const user = await db.collection("doceaseclients").get(email);
 
     if (!user) {
-      return res.status(200).json({
+      return res.status(400).json({
         success: false,
         message: "There is no user with supplied email",
       });
@@ -208,9 +213,6 @@ app.post("/users/reset-password/:key/:token", async (req, res) => {
         success: false,
         message: "Please provide the reset token",
       });
-    const hashedToken = createHash("sha256").update(token).digest("hex");
-
-    console.log("hashedToken resetToken ", hashedToken);
 
     const user = await db.collection("doceaseclients").get(key);
 
@@ -220,6 +222,7 @@ app.post("/users/reset-password/:key/:token", async (req, res) => {
         message: "We could'nt that matches provided key",
       });
     }
+    const hashedToken = createHash("sha256").update(token).digest("hex");
     const savedToken = user.props?.passwordResetToken;
 
     if (hashedToken !== savedToken) {
@@ -263,11 +266,10 @@ app.post("/users/reset-password/:key/:token", async (req, res) => {
       .json({ success: true, message: "password reset successful" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: "Internal server error." });
+    res.status(500).json({ success: true, message: "Internal server error." });
   }
 });
 
-// TODO: To include protection check
 // update profile
 app.patch("/users/update-profile", authorize, async (req, res) => {
   const key = res.locals.key;
@@ -337,35 +339,6 @@ app.patch("/users/update-password", authorize, async (req, res) => {
     .status(200)
     .json({ success: true, message: "Password changed successfully" });
 });
-
-async function authorize(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  let token;
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    token = authHeader.split(" ")[1];
-  }
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "You are not logged! Please to get access",
-    });
-  }
-  const jwtSecret = "wzPe7g19Yan27T2ATud1Kw==";
-
-  const decoded = jwt.verify(token, jwtSecret);
-
-  const user = await db.collection("doceaseclients").get(decoded.key);
-
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "The user belonging to this token no exists!",
-    });
-  }
-
-  res.locals.key = decoded.key;
-  next();
-}
 
 app.post("/users/profile-picture", authorize, uploadFile, async (req, res) => {
   try {
@@ -474,10 +447,34 @@ app.patch("/users/profile-picture", authorize, uploadFile, async (req, res) => {
   }
 });
 
-app.get("/user/feedback", async (req, res) => {
-  const feedback = req.body.feedback;
-  console.log("feedback", feedback);
-});
+async function authorize(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  let token;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "You are not logged! Please to get access",
+    });
+  }
+  const jwtSecret = "wzPe7g19Yan27T2ATud1Kw==";
+
+  const decoded = jwt.verify(token, jwtSecret);
+
+  const user = await db.collection("doceaseclients").get(decoded.key);
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "The user belonging to this token no exists!",
+    });
+  }
+
+  res.locals.key = decoded.key;
+  next();
+}
 
 app.get("/:col/:key", async (req, res) => {
   // getting a key from a collection
@@ -504,8 +501,120 @@ app.get("/:col", async (req, res) => {
   res.json(items).end();
 });
 
-const PORT = 5000 || process.env.PORT;
+app.post("collection/:colName", async (req, res) => {
+  // creating a collection
+  const { colName } = req.params;
+  const newCollection = await db.collection(colName);
+  console.log(JSON.stringify(newCollection, null, 2));
+  res.json(newCollection).end();
+});
 
-app.listen(PORT, () => {
-  console.log("server started on port " + PORT);
+app.delete("collection/:colName", async (req, res) => {
+  // deleting a collection
+  const { colName } = req.params;
+  // try{}
+  await db.delete(colName);
+  console.log(JSON.stringify(newCollection, null, 2));
+  res.json(newCollection).end();
+});
+
+app.post("/login", async (req, res) => {
+  // login
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Both email and password are required.",
+    });
+  }
+
+  try {
+    // Fetch the client data from the database using the provided email
+    const student = await db.collection("doceaseclients").get(email);
+
+    // If the student doesn't exist or passwords don't match, return an error
+    if (!student || student.props.password !== password) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password." });
+    }
+
+    // If everything is okay, return a success message (and possibly a token or any other data)
+    res.json({
+      success: true,
+      message: "Login successful.",
+      data: { firstname: student.props.firstname },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+});
+
+// deleting a user if they exist
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await db.collection("doceaseclients").delete(id);
+    res.json({ success: true, message: "User deleted successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Fetching the client with the provided email
+    const client = await db.collection("doceaseclients").get(email);
+    if (client) {
+      const passwordMatch = await bcrypt.compare(
+        password,
+        client.props.password
+      );
+      if (!passwordMatch) {
+        return res
+          .status(401)
+          .json({ success: false, message: "Invalid password entered." });
+      } else {
+        // return res.status(401).json({ success: false, message: 'Invalid email or password.' });
+        const accessToken = signAccessToken(client.key);
+        return res.status(200).json({
+          success: true,
+          message: "Login successful.",
+          data: { client },
+          accessToken: accessToken,
+        });
+      }
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: `Email adress not found, if you don't have an account please sign up.`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+});
+
+// verifying if email already exists in the database
+app.post("/users/verify", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const student = await db.collection("doceaseclients").get(email);
+    res.json({
+      success: true,
+      message: "User verified successfully.",
+      data: { exists: !!student },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
 });
